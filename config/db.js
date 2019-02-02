ef86cf2801e4fb24796d72e95eb9b9d3cb677d79
Heeyeong Kim <gldud9833@gmail.com>
2019-02-02 12:22:44 -0500

    added mLab support

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     // local MongoDB deployment
-    "URI": "mongodb://localhost/demo"
+    //"URI": "mongodb://localhost/demo"
+    //"URI": "mongodb://ellie:a123456@ds050739.mlab.com:50739/comp308-w2019-lesson4a"
 }
